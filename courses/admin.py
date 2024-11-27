237576b9095219dc7e1e4191d1011e02c5e4aa36
praveen <praveenrajavel@Praveens-MacBook-Air.local>
2024-11-27 14:51:10 +0000

    2

--- courses/admin.py
+++ courses/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, WeeklyModule, Enrollment
 
-admin.site.register(Course)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'instructor', 'category', 'level', 'created_at')
+    list_filter = ('level', 'category', 'instructor')
+    search_fields = ('title', 'description')
+    ordering = ('created_at',)
+
+
+@admin.register(WeeklyModule)
+class WeeklyModuleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course', 'order')
+    list_filter = ('course',)
+    search_fields = ('title', 'description')
+    ordering = ('course', 'order')
+
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course', 'progress', 'enrolled_on')
+    list_filter = ('course', 'student')
+    search_fields = ('student__username', 'course__title')
+    ordering = ('enrolled_on',)
